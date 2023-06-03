9ef55d8237156eb141e42d0e02a171dc6426bdb8
MisterChpunk <134156884+MisterChpunk@users.noreply.github.com>
2023-06-03 18:05:04 +0200

    Update main.py
    Дорогие игроки, в этом обновлении мы добавили отскок мяча от ракеток и отскок от верхней и нижней стенки.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from pygame import*
-
+from pygame import *
 # базовый класс для спрайтов
 class GameSprite(sprite.Sprite):
     """
@@ -8,7 +7,7 @@ class GameSprite(sprite.Sprite):
     y - координата y спрайта
     speed - скорость спрайта
     size_x - размер спрайта по горизонтали
-    size_y - размер спрайта по вертикали
+    size_y - размер спрайта по в ертикали
     """
 
     def __init__(self, image_file, x, y, speed, size_x, size_y):
@@ -40,7 +39,7 @@ class Player(GameSprite):
             self.rect.y -= self.speed
 
         # если нажата клавиша вправо и физическая модель не ушла за правую границу игры
-        if keys[K_DOWN] and self.rect.y < width - 70:
+        if keys[K_DOWN] and self.rect.y < height - 150:
             self.rect.y += self.speed
 
     def update_l(self):
@@ -52,7 +51,7 @@ class Player(GameSprite):
             self.rect.y -= self.speed
 
         # если нажата клавиша вправо и физическая модель не ушла за правую границу игры
-        if keys[K_s] and self.rect.y < width - 70:
+        if keys[K_s] and self.rect.y < height - 150:
             self.rect.y += self.speed
 
 # переменная окончания игры
@@ -61,7 +60,7 @@ finish = False  # когда True, то спрайты перестают раб
 game = True  # завершается при нажатии кнопки закрыть окно
 
 # размеры окна
-width = 700
+width = 600
 height = 500
 
 # создание окна
@@ -69,20 +68,22 @@ window = display.set_mode((width, height))
 display.set_caption("Ping Pong")
 back = (200, 255, 255)
 window.fill(back)
-clock =  time.Clock()
+clock = time.Clock()
 FPS = 60
+
 
 # шрифт
 font.init()
 font1 = font.SysFont("Arial", 36)
-lose1 = font1.render('PLAYER 1 LOSE!', True, (180,0,0))
-lose2 = font1.render('PLAYER 2 LOSE!', True, (180,0,0))
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
-racket1 = Player("racket.png", 30,200,4,50,150)
-racket2 = Player("racket.png", 520,200,4,50,150)
-ball = GameSprite("tenis_ball.png", 200,200,4,50,50)
+racket1 = Player("derevo.png", 30, 200, 4, 50, 150)
+racket2 = Player("derevo.png", 520, 200, 4, 50, 150)
+ball = GameSprite('stive.jpg', 200, 200, 4, 50, 50)
 ball_x = 3
 ball_y = 3
+
 
 # игровой цикл
 while game:
@@ -95,13 +96,30 @@ while game:
         window.fill(back)
         racket1.update_l()
         racket2.update_r()
+
         ball.rect.x += ball_x
         ball.rect.y += ball_y
+
+        if sprite.collide_rect(racket1,ball):
+            ball_x *= -1
+
+        if sprite.collide_rect(racket2,ball):
+            ball_x *= -1
+
+        if ball.rect.y < 0 or ball.rect.y > height - 50:
+            ball_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1,(200,200))
+
+        if ball.rect.x > width - 50:
+            finish = True
+            window.blit(lose2,(200,200))
 
         racket1.reset()
         racket2.reset()
         ball.reset()
-
-
+        
     display.update()
     clock.tick(FPS)
